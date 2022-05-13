b29234d3110e1cf3aabb861e2a30bf3478132e5c
Sourav Kumar <42813823+souravkumardtu@users.noreply.github.com>
2022-05-13 14:45:25 +0530

    Update movie-page.js

--- movie-page.js
+++ movie-page.js
@@ -35,7 +35,7 @@ for(let i = 0 ; i < s.length ;i++){
 // console.log(r);
 
 
-var PromiseApi = fetch(`http://www.omdbapi.com/?apikey=89239137&t=${r}`).then(function(res){
+var PromiseApi = fetch(`https://www.omdbapi.com/?apikey=89239137&t=${r}`).then(function(res){
                 return res.json();})
                 .then(function(data){
                     // console.log(data);
